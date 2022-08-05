597d0f2b388a2ce2271c595082a8eaf5e209532e
eltigro1999 <eltigrolearning@gmail.com>
2022-08-05 19:02:10 +0300

    2 Cmakelists and 2 targets added

---
+++ SimpleEngineCore/src/SimpleEngineCore/Utils/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "SimpleEngineCore/Utils/test.hpp"
+
+//namespace SimpleEngine {
+//	void SayHello() {
+//		std::cout << "Hello from Simple engine core"<<std::endl;
+//	}
+//}
+
+void SimpleEngine::SayHello() {
+	std::cout << "Hello from Simple engine core" << std::endl;
+}
---
+++ SimpleEngineEditor/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "SimpleEngineCore/Utils/test.hpp"
+
+int main() {
+	std::cout << "Hello from simple engine editor!" << std::endl;
+	SimpleEngine::SayHello();
+	return 0;
+}
