72840d19f31c93d4983301abac3a983e5e93854e
eltigro1999 <eltigrolearning@gmail.com>
2022-08-10 20:11:55 +0300

    Event types are added

--- SimpleEngineCore/includes/SimpleEngineCore/Event.hpp
+++ SimpleEngineCore/includes/SimpleEngineCore/Event.hpp
@@ -1,6 +1,21 @@
 #pragma once
 
 namespace SimpleEngine {
+
+	enum class EventType {
+		WindowResize=0,
+		WindowClose,
+
+		KeyPressed,
+		KeyReleased,
+
+		MouseButtonPressed,
+		MouseButtonReleased,
+		MouseMoved,
+
+		EventsCount
+	};
+
 	struct Event{
 		unsigned int width;
 		unsigned int height;
