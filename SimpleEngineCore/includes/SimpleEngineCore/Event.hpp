659af72e82f75dca2dbaca718a9cbf4ada4e9609
eltigro1999 <eltigrolearning@gmail.com>
2022-08-10 20:20:25 +0300

    Mouse Moved event added

--- SimpleEngineCore/includes/SimpleEngineCore/Event.hpp
+++ SimpleEngineCore/includes/SimpleEngineCore/Event.hpp
@@ -16,6 +16,23 @@ namespace SimpleEngine {
 		EventsCount
 	};
 
+	struct BaseEvent {
+		virtual ~BaseEvent() = default;
+		virtual EventType get_type() const = 0;
+	};
+
+	struct EventMouseMoved : public BaseEvent {
+		EventMouseMoved(const double& new_x, const double& new_y) :
+			x(new_x), y(new_y) {}
+
+
+		virtual EventType get_type() const override { return type; }
+
+		static const EventType type=EventType::MouseMoved;
+		double x;
+		double y;
+	};
+
 	struct Event{
 		unsigned int width;
 		unsigned int height;
