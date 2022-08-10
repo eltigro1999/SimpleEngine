a7eb2f88be298786b25596606908d0f8bed99aea
eltigro1999 <eltigrolearning@gmail.com>
2022-08-10 20:23:58 +0300

    Window resize event is added

--- SimpleEngineCore/includes/SimpleEngineCore/Event.hpp
+++ SimpleEngineCore/includes/SimpleEngineCore/Event.hpp
@@ -33,6 +33,20 @@ namespace SimpleEngine {
 		double y;
 	};
 
+	struct EventWindowResize : public BaseEvent {
+		EventWindowResize(const unsigned int& new_width, const unsigned int& new_height) :
+			width(new_width), height(new_height) {}
+
+
+		virtual EventType get_type() const override { return type; }
+
+		static const EventType type = EventType::WindowResize;
+		unsigned int width;
+		unsigned int height;
+	};
+
+
+
 	struct Event{
 		unsigned int width;
 		unsigned int height;
